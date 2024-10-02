8e40d51db17e3d9eaf1b922f1018fec2d8cad683
Dibman1 <malthe@dibbern.dk>
2024-10-02 23:34:19 +0200

    Test4

--- Padlock.gd
+++ Padlock.gd
@@ -1,15 +1,15 @@
 extends RigidBody3D
 
-var LockState1 = 3
-var LockState2 = 7
-var LockState3 = 5
+var LockState1 = 0
+var LockState2 = 0
+var LockState3 = 0
 var LockState4 = 0
 var open = 0
 signal lock_open(value)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	while open == 0:
+	while open == 1:
 		if LockState1 == 3:
 			if LockState2 == 7:
 				if LockState3 == 5:
